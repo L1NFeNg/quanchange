dcc5784bf1a549aa2a9d8a1bbd6cd708499f45a7
lf <332373135@qq.com>
2020-04-09 22:37:41 +0800

    券易后端项目骨架

---
+++ quanchange-gateway/src/main/java/com/quanchange/QuanchangeGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.quanchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class QuanchangeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanchangeGatewayApplication.class);
+
+    }
+}
